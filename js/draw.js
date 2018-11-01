c9d61e783f5b85e64b05c124eab9e8caae71ef02
Anndo Ko <handcuffanndo@gmail.com>
2018-11-01 18:43:48 -0400

    Clean up the code

--- js/draw.js
+++ js/draw.js
@@ -1,97 +1,91 @@
+var reverse = false;
 var testData = [
   { name: "Task 1", size: 10, distance: 160 },
   { name: "Task 2", size: 60, distance: 160 },
   { name: "Task 3", size: 150, distance: 160 },
   { name: "Task 4", size: 200, distance: 160 }
 ];
-
 var testResults = [];
 
 $(document).ready(function () {
   loadTestData();
   loadData();
-
 });
 
 // ---------- Fitts Interaction -----
 function loadTestData() {
   var i = 0;
-  var button = d3.select("#reverse");
 
   drawTarget(i);
 
   // Reverse button event
-  var reverse = false;
-  button.on("click", function(){
-    console.log("old:", reverse);
-    reverse = !reverse;
-    console.log("new:", reverse);
-      if (reverse){
-        $("#interaction").empty();
-        drawTargetReverse(i);
-      } else {
-        $("#interaction").empty();
-        drawTarget(i);
-      }
+  d3.select("#reverse").on("click", function(){
+    reverseButtons(i);
   });
-
-
 }
 
-// Default buttons
-function drawTarget(i){
+// Draw interactive buttons
+function drawTarget(i, reverse){
   var body = d3.select("#interaction")
   var margin = { top: 50, right: 50, bottom: 50, left: 50 }
   var height = 300 - margin.top - margin.bottom
   var width = 800 - margin.left - margin.right
-  var w = testData[i]["size"]
-  var a = testData[i]["distance"]
 
   var svgContainer = body.append("svg")
     .attr("height", height + margin.top + margin.bottom)
     .attr("width", width + margin.left + margin.right)
 
-  var target1 = svgContainer.append("rect")
-    .attr("id", "target1")
-    .attr("x", 0)
-    .attr("y", 24)
-    .attr("width", w)
-    .attr("height", 250)
-    .attr("disabled", true)
-    .attr("fill", "grey")
+  var w = testData[i]["size"]
+  var a = testData[i]["distance"]
 
-  var target2 = svgContainer.append("rect")
-    .attr("id", "target2")
-    .attr("x", w + a)
-    .attr("y", 24)
-    .attr("width", w)
-    .attr("height", 250)
-    .attr("disabled", false)
-    .attr("fill", "red")
+  if(!reverse){
+    var target1 = svgContainer.append("rect")
+      .attr("id", "target1")
+      .attr("x", 0)
+      .attr("y", 24)
+      .attr("width", w)
+      .attr("height", 250)
+      .attr("disabled", true)
+      .attr("fill", "grey")
+
+    var target2 = svgContainer.append("rect")
+      .attr("id", "target2")
+      .attr("x", w + a)
+      .attr("y", 24)
+      .attr("width", w)
+      .attr("height", 250)
+      .attr("disabled", false)
+      .attr("fill", "red")
+
+  } else {
+    var target1 = svgContainer.append("rect")
+      .attr("id", "target1")
+      .attr("x", 0)
+      .attr("y", 24)
+      .attr("width", 250)
+      .attr("height", w)
+      .attr("disabled", true)
+      .attr("fill", "grey")
+
+    var target2 = svgContainer.append("rect")
+      .attr("id", "target2")
+      .attr("x", 0)
+      .attr("y", 24 + w + a)
+      .attr("width", 250)
+      .attr("height", w)
+      .attr("disabled", false)
+      .attr("fill", "red")
+  }
 
   curr_time = Date.now(); // Set the timer
 
-  // Target 1 events
   target1.on("click", function(){
-
     // Stop and reset the timer
     if (d3.select("#target1").attr("disabled") == "false") {
       next_time = Date.now();
-      move_time = next_time - curr_time;
-      console.log("Click 1:", move_time);
-
-      var data = [];
-      data.push(move_time);
-      data.push(w);
-      data.push(a);
-      console.log(data);
-      testResults.push(data);
-      data = [];
-      console.log("Print 1:", testResults);
-
+      saveResult(curr_time, next_time, a, w);
       curr_time = Date.now();
     }
-
     // Switch button status and colors
     target1
       .attr("disabled", true)
@@ -99,27 +93,15 @@ function drawTarget(i){
     target2
       .attr("disabled", false)
       .attr("fill", "red")
-
   });
 
   target2.on("click", function(){
-
     // Stop and reset the timer
     if (d3.select("#target2").attr("disabled") == "false") {
       next_time = Date.now();
-      move_time = next_time - curr_time;
-      console.log("Click 2:", move_time);
-      var data = [];
-      data.push(move_time);
-      data.push(w);
-      data.push(a);
-      console.log(data);
-      testResults.push(data);
-      data = [];
-      console.log("Print 2:", testResults);
+      saveResult(curr_time, next_time, a, w);
       curr_time = Date.now();
     }
-
     // Switch button status and colors
     target2
       .attr("disabled", true)
@@ -130,92 +112,19 @@ function drawTarget(i){
   });
 }
 
-// Reversed buttons
-function drawTargetReverse(i){
-  var body = d3.select("#interaction")
-  var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-  var height = 300 - margin.top - margin.bottom
-  var width = 800 - margin.left - margin.right
-  var w = testData[i]["size"]
-  var a = testData[i]["distance"]
+// Reverse buttons
+function reverseButtons(i){
+  reverse = !reverse;
+  $("#interaction").empty();
+  drawTarget(i, reverse);
+}
 
-  var svgContainer = body.append("svg")
-    .attr("height", height + margin.top + margin.bottom)
-    .attr("width", width + margin.left + margin.right)
-
-  var target1 = svgContainer.append("rect")
-    .attr("id", "target1")
-    .attr("x", 0)
-    .attr("y", 10)
-    .attr("width", 250)
-    .attr("height", w)
-    .attr("disabled", true)
-    .attr("fill", "grey")
-
-  var target2 = svgContainer.append("rect")
-    .attr("id", "target2")
-    .attr("x", 0)
-    .attr("y", 10 + w + a)
-    .attr("width", 250)
-    .attr("height", w)
-    .attr("disabled", false)
-    .attr("fill", "red")
-
-  curr_time = Date.now(); // Set the timer
-
-  // Target 1 events
-  target1.on("click", function(){
-
-    // Stop and reset the timer
-    if (d3.select("#target1").attr("disabled") == "false") {
-      next_time = Date.now();
-      move_time = next_time - curr_time;
-      console.log("Click 1:", move_time);
-
-      var data = [];
-      data.push(move_time);
-      data.push(w);
-      data.push(a);
-      console.log(data);
-      testResults.push(data);
-      data = [];
-      console.log("Print:", testResults);
-
-
-      curr_time = Date.now();
-    }
-
-    // Switch button status and colors
-    target1
-      .attr("disabled", true)
-      .attr("fill", "grey")
-    target2
-      .attr("disabled", false)
-      .attr("fill", "red")
-
-  });
-
-  // Target 2 events
-  target2.on("click", function(){
-
-    // Stop and reset the timer
-    if (d3.select("#target2").attr("disabled") == "false") {
-      next_time = Date.now();
-      move_time = next_time - curr_time;
-      console.log("Click 2:", move_time);
-      testResults.push(move_time);
-      console.log("Print:", testResults);
-      curr_time = Date.now();
-    }
-
-    // Switch button status and colors
-    target2
-      .attr("disabled", true)
-      .attr("fill", "grey")
-    target1
-      .attr("disabled", false)
-      .attr("fill", "red")
-  });
+// Save the user's click
+function saveResult(curr_time, next_time, a, w){
+  var data = []
+  data.push(Math.log2(a/w + 1), next_time - curr_time);
+  testResults.push(data);
+  console.log("Clicked & Saved: ", testResults);
 }
 
 // ---------- Fitts Visualization -----
@@ -229,8 +138,8 @@ function loadData() {
 
     var xScale = d3.scale.linear()
       .domain([
-      	d3.min([0, d3.min(data,function (d) { return d.IndexofDifficulty1 })]),
-      	d3.max([0, d3.max(data,function (d) { return d.IndexofDifficulty1 })])
+      	d3.min([0, d3.min(data,function (d) { return d.IndexofDifficulty2 })]),
+      	d3.max([0, d3.max(data,function (d) { return d.IndexofDifficulty2 })])
       	])
       .range([0,w])
 
@@ -271,7 +180,7 @@ function loadData() {
       .delay(function(d, i) {
         return i / data.length * 500;
       })
-      .attr("cx",function (d) { return xScale(d.IndexofDifficulty1) })
+      .attr("cx",function (d) { return xScale(d.IndexofDifficulty2) })
       .attr("cy",function (d) { return yScale(d.Mean) })
 
     svg.append("g")
